2519f03f8ec3571a6dd132aa40c40932d6c9fa87
rajyotiroshan <rajyotiroshan@gmail.com>
2024-04-15 14:41:35 +0530

    Anonymous delegates

--- Start/Delegates/AnonymousDelegates/Program.cs
+++ Start/Delegates/AnonymousDelegates/Program.cs
@@ -9,6 +9,12 @@ namespace AnonymousDelegates
         static void Main(string[] args)
         {
             // TODO: Implement an anonymous delegate
+            MyDelegate f = delegate(int arg1, int arg2){ //declare function right in here 
+                return (arg1 + arg2).ToString();
+            };
+
+            Console.WriteLine("Thhe result is " + f(10, 20));
+
 
         }
     }
