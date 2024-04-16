59e69a70190cc69a69cbccf3eb8e075efd10084b
rajyotiroshan <rajyotiroshan@gmail.com>
2024-04-16 14:20:38 +0530

    Basic Lambda done

--- Start/Delegates/DelegatesSolution/Program.cs
+++ Start/Delegates/DelegatesSolution/Program.cs
@@ -2,10 +2,17 @@
 
 namespace DelegatesSolution
 {
+    public delegate int ShippingCharge(string zone);
     class Program
     {
         static void Main(string[] args)
         {
+            
+
+         while("exit" != Console.ReadLine()){
+            string zoneName =Console.ReadLine();
+            Console.WriteLine(zoneName);
+         }
         }
     }
 }
